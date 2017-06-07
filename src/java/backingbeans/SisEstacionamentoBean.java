eac65efc52400c5aa977ac18521414f861d1b20a
sergio <sergio.gambarra@restinga.ifrs.edu.br>
2017-06-07 20:06:44 -0300

    criando barra de progresso upload servidores

--- src/java/backingbeans/SisEstacionamentoBean.java
+++ src/java/backingbeans/SisEstacionamentoBean.java
@@ -251,6 +251,34 @@ public class SisEstacionamentoBean implements Serializable{
         return strategy;
     }
     
+    private Integer progress;
+ 
+    public Integer getProgress() {
+        if(progress == null) {
+            progress = 0;
+        }
+        else {
+            progress = progress + (int)(Math.random() * 35);
+             
+            if(progress > 100)
+                progress = 100;
+        }
+         
+        return progress;
+    }
+ 
+    public void setProgress(Integer progress) {
+        this.progress = progress;
+    }
+     
+    public void onComplete() {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Progress Completed"));
+    }
+     
+    public void cancel() {
+        progress = null;
+    }
+    
      
     
         /**
